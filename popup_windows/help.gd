6e23eba74ee2e4fd1763859c26b1d5c29197f2f9
wiechciu <wiechciu@gmail.com>
2025-07-05 00:28:43 +0200

    Added version number and release settings

--- popup_windows/help.gd
+++ popup_windows/help.gd
@@ -3,11 +3,13 @@ extends Control
 
 
 @export var ok_button: Button
+@export var version_label: Label
 
 
 func _ready() -> void:
 	ok_button.pressed.connect(_on_ok_button_pressed)
 	ok_button.grab_focus()
+	version_label.text = "v%s" % ProjectSettings.get_setting("application/config/version")
 
 
 func _on_ok_button_pressed() -> void:
